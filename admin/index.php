25d35fe614b66bad44ed427ae4ed75417837b1c6
Evools <jefr.p@yandex.ru>
2025-01-18 16:10:38 +0600

    create admin panel

---
+++ admin/index.php
@@ -0,0 +1,10 @@
+<?php include "admin/layout/head.php"; ?>
+<?php include "admin/layout/nav.php"; ?>
+<div class="p-4">
+
+    <?php include "admin/include/breadcrumb.php"; ?>
+
+    <?php include "admin/include/platform.php"; ?>
+</div>
+<?php include "admin/layout/footer.php"; ?>
+
